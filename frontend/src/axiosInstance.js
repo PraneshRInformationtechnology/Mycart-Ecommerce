31dff7179e0269039f5981c6e40eaca3516cf8be
pranesh <r.pranesh001@gmail.com>
2025-04-24 19:09:34 +0530

    final commit

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://mycart-ecommerce.onrender.com',
   withCredentials: true, // only if you're using cookies
 });
+console.log("Base URL:", process.env.REACT_APP_API_URL);
 
 export default instance;
