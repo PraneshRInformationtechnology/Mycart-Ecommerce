9d4947c2fe1df07aac5a4094f2ed865b196ca51f
pranesh <r.pranesh001@gmail.com>
2025-04-25 13:32:57 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,8 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads', express.static(path.join(__dirname,'uploads') ) )
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 const products = require('./routes/product')
 const auth = require('./routes/auth')
